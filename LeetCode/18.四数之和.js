c9c3b3e897672f345d9cf95746b9880d83019038
仇诗龙 <qiushl@haplox.com>
2022-09-05 08:36:30 +0800

    ddu: 22.9.5

--- LeetCode/18.四数之和.js
+++ LeetCode/18.四数之和.js
@@ -13,20 +13,17 @@
 var fourSum = function (nums, target) {
 	if (nums.length < 4) return [];
 
-	const result = [];
+	let result = [];
 
 	nums = nums.sort((a, b) => a - b);
 
-	for (let i = 0; i < nums.length; i = addToNoRepet(nums, i, nums.length - 4)) {
-		if (nums[i] > target) return result;
-		let l = i + 1;
-		let r = nums.length - 2;
-		let last = nums.length - 1;
-
-		while (l < r) {
-			sum = nums[i] + nums[last] + nums[l] + nums[r];
-			sum === target && result.push([nums[i], nums[l], nums[r], nums[last]]);
-			sum <= target ? (l = addToNoRepet(nums, l, r)) : r--;
+	for (let i = 0; i < nums.length; i++) {
+		for (let j = 0; j < nums.length; j++) {
+			const tempResult = twoPointReasult(nums, i, j, target);
+			console.log(i, j, tempResult);
+			if (tempResult.length > 0) {
+				result = [...result, ...tempResult];
+			}
 		}
 	}
 	return result;
