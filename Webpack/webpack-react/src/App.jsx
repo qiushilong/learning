a5326d2e92bb8fed774deca33d5355ea21ac2d09
shilong <1490683763@qq.com>
2022-09-12 23:21:00 +0800

    ddu: 22.9.12

---
+++ Webpack/webpack-react/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Home from "./Home";
+
+function App() {
+  return (
+    <>
+      <h1>App</h1>
+      <div>
+        <Home></Home>
+      </div>
+    </>
+  );
+}
+
+export default App;
